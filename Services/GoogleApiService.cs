b3daa062decd649cc26c6beb2292540c82e34670
sanzh <sanzhar285@gmail.com>
2023-02-08 11:47:36 +0600

    added services

---
+++ Services/GoogleApiService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using UploadGoogleDrive.Controllers;
+
+namespace UploadGoogleDrive.Services
+{
+	public class GoogleApiService : IGoogleApiService
+	{
+        public string PathToServiceAccountKeyFile { get; set; }
+        public DriveService Service { get; set; }
+        public void DoSomething()
+        {
+            var credential = GoogleCredential.FromFile(PathToServiceAccountKeyFile)
+                .CreateScoped(DriveService.ScopeConstants.Drive);
+            DriveService service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential
+            });
+        }
+    }
+}
+
---
+++ Services/IGoogleApiService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Google.Apis.Drive.v3;
+
+namespace UploadGoogleDrive.Services
+{
+	public interface IGoogleApiService
+	{
+        string PathToServiceAccountKeyFile { get; set; }
+        DriveService Service { get; set; }
+        void DoSomething();
+    }
+}
+
